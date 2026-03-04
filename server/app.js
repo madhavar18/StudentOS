d84c35d4d3314ab9fe8178093f44934ced78de8d
Madhava <madhavarao1817@gmail.com>
2026-03-04 20:28:23 +0530

    minor change : added cors module for frontend and backend integration

--- server/app.js
+++ server/app.js
@@ -1,10 +1,12 @@
 require("dotenv").config();
 
 const express = require("express");
+const cors  = require("cors");
 const taskRoutes = require("./routes/tasks");
 const db = require("./config/db");
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use("/tasks", taskRoutes);
 
